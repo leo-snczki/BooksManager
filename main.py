14069b429e9272924b47ed0335cb9bd71349ad9b
lsz-edu <79675019+leo-snczki@users.noreply.github.com>
2025-05-08 15:19:51 +0100

    ✨ Adicionado "main.py" com menu de seleção, além do uso da biblioteca 'readkeys'.

---
+++ main.py
@@ -0,0 +1,42 @@
+from clear import clear_term
+from readkeys import getch
+
+def MostrarMenu():
+    opcoes = [
+        "1 - Criar livro",
+        "2 - Ver livros",
+        "3 - Cadastrar leitor",
+        "4 - Ver leitores",
+        "5 - Alterar status de um livro",
+        "6 - Sair"
+    ]
+    
+    print("\n".join(opcoes))
+    print("\nOpção: ", end="")
+
+def Menu (opcao):
+    match opcao:
+        case "1":
+            return True # Lógica não implementada ainda
+        case "2":
+            return True # Lógica não implementada ainda
+        case "3":
+            return True # Lógica não implementada ainda
+        case "4":
+            return True # Lógica não implementada ainda
+        case "5":
+            return True # Lógica não implementada ainda
+        case "6":
+            return False
+        case _:
+            print("\nOpção inválida.\n")
+            getch(print("Pressione qualquer tecla para tentar novamente..."))
+            return True
+
+continuar = True
+
+while(continuar):
+    clear_term()
+    MostrarMenu()
+    opcao = str(input())
+    continuar = Menu(opcao)
