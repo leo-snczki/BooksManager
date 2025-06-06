423ce715330239ef3586ec15ceb2a657d5cc857b
lsz-edu <79675019+leo-snczki@users.noreply.github.com>
2025-06-06 10:55:44 +0100

    🐛 Nome errado da propriedade da classe no print para utilzador.

--- gestorlivros.py
+++ gestorlivros.py
@@ -25,6 +25,7 @@ class gestorLivros:
                 emprestimo = EmprestimoLivro(livro, leitor)
                 self.emprestimos.append(emprestimo)
                 return f'O livro "{livro.titulo}" foi emprestado com sucesso (Devolução: {emprestimo.data_devolucao.strftime("%d/%m/%Y")}).'
+                return f'O livro "{livro.titulo}" foi emprestado com sucesso (Devolução: {emprestimo.data_para_devolucao.strftime("%d/%m/%Y")}).'
         return "Livro não disponível ou não encontrado."
     
     def carregar_livros_json(self, arquivo):
